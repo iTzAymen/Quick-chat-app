30d6c89e86a558bdf0564ad601041dbf8e406294
iTzAymen <aymeno190@gmail.com>
2023-04-14 17:56:21 +0100

    second commit

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        richblack: "#030407",
+        richblack2: "#0A0E18",
+        richblack3: "#0F1524",
+        brilliantazure: "#2FA1F9",
+        brilliantazure2: "#025697",
+        brilliantazure3: "#52B1FA",
+        brilliantazure4: "#0877CD",
+        babyblue: "#91CAF6",
+        babyblue2: "#B7DCF8",
+      },
+    },
   },
   plugins: [],
 };
+
+// npx tailwindcss -i ./styles.css -o ./dist/output.css --watch
